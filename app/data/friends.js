ceaa1a713ed17f90563cc46f3d22c447f135da5a
Brooksbot <chrisbailey@Chriss-MacBook-Air.local>
2018-08-08 00:13:14 -0500

    added friends.js content

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+// Data below data will hold all of the possible friends.
+var friends = [
+    {
+      name: "Mr. Bob",
+      photo: "https://vignette.wikia.nocookie.net/king-harkinian/images/4/47/Spongebob.png/revision/latest?cb=20170518185222",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Fred Hamn",
+      photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fimages.tritondigitalcms.com%2F6616%2Fsites%2F389%2F2018%2F02%2F15072437%2FJoseCansecoAP.jpg&imgrefurl=http%3A%2F%2Fwww.wjbc.com%2F2018%2F02%2F15%2Fcornbelters-sign-canseco-to-3-day-contract%2F&docid=XeWcf6WTaywOJM&tbnid=ZdvFjgEaDWCtLM%3A&vet=10ahUKEwi7gZ3e0dzcAhUMEawKHYIXAowQMwitAigQMBA..i&w=630&h=420&bih=570&biw=1146&q=jose%20canseco%20pictures&ved=0ahUKEwi7gZ3e0dzcAhUMEawKHYIXAowQMwitAigQMBA&iact=mrc&uact=8",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Ariana Grande",
+      photo: "https://www.theguardian.com/music/2018/aug/02/mac-miller-swimming-review#img-1",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Shu Kurenai",
+      photo: "https://vignette.wikia.nocookie.net/beyblade/images/c/c0/DA-6YAbVoAAstnH.jpg/revision/latest?cb=20170529220734",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Free De La Hoya",
+      photo: "https://pm1.narvii.com/6671/92f33103eae48abe665560bacbfc825d1f4f421b_hq.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Adam Freedom",
+      photo: "https://www.factinate.com/wp-content/uploads/2017/04/4-8.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Export the array. This makes it accessible to other files using require.
+  module.exports = friends;
